0f6646283ad7f56c84b557098cd6bf499c38d100
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-18 14:13:08 -0400

    refactored online users component

--- src/components/Chat/ChatMessage/index.js
+++ src/components/Chat/ChatMessage/index.js
@@ -8,8 +8,8 @@ function ChatMessage({userName, time, message}) {
             <div className={styles["chat-avatar-wrapper"]}>
                 <img src="https://via.placeholder.com/200" alt="Chat Avatar" className={styles["chat-avatar"]} />
             </div>
-            <div className="chat-message-content ml-2">
-                <div className="chat-message-info mb-2">
+            <div className={styles["chat-message-content"]}>
+                <div className={styles["chat-message-info"]}>
                     <span>{userName} <span>{moment(time).from(Date.now())}</span></span>
                 </div>
                 <p>{message}</p>
