6c7028026cb34eed63c85e9e2ed77a0b2ef97d08
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-18 11:20:16 -0400

    changes messages to load when component did mount; in prep for getting from database

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,89 +1,105 @@
-import React from 'react';
-import ChatMessage from './ChatMessage';
-import './style.css';
+import React from "react";
+import ChatMessage from "./ChatMessage";
+import "./style.css";
 
 class ChatBox extends React.Component {
-    state = {
-        chatInput: "",
-        usersOnline: [
-            {
-                userName: "Username123",
-                avatarImg: "https://via.placeholder.com/100",
-                socketId: ""
-            }
-        ],
-        messages: [
-            {
-                userName: "Username123",
-                time: Date.now(),
-                message: "Here's my message to the chat box."
-            },
-            {
-                userName: "Username1234",
-                time: Date.now(),
-                message: "Here's my message to the chat box."
-            },
-            {
-                userName: "Username1234",
-                time: Date.now(),
-                message: "Here's my message to the chat box."
-            },
-            {
-                userName: "Username1234",
-                time: Date.now(),
-                message: "Here's my message to the chat box."
-            },
-            {
-                userName: "Username1234",
-                time: Date.now(),
-                message: "Here's my message to the chat box."
-            }
-            
-        ]
-    }
+  state = {
+    chatInput: "",
+    usersOnline: [
+      {
+        userName: "Username123",
+        avatarImg: "https://via.placeholder.com/100",
+        socketId: ""
+      }
+    ],
+    messages: []
+  };
 
-    handleInputChange = event => {
-        const {value} = event.target;
+  componentDidMount = () => {
+    // Get the chat messages from the database as axios
+    // For now, dummy messages.
+    this.setState({
+      messages: [
+        {
+          userName: "Username123",
+          time: Date.now(),
+          message: "Here's my message to the chat box."
+        },
+        {
+          userName: "Username1234",
+          time: Date.now(),
+          message: "Here's my message to the chat box."
+        },
+        {
+          userName: "Username1234",
+          time: Date.now(),
+          message: "Here's my message to the chat box."
+        },
+        {
+          userName: "Username1234",
+          time: Date.now(),
+          message: "Here's my message to the chat box."
+        },
+        {
+          userName: "Username1234",
+          time: Date.now(),
+          message: "Here's my message to the chat box."
+        }
+      ]
+    });
+  };
 
-        this.setState({
-            chatInput: value
-        });
-    }
+  handleInputChange = event => {
+    const { value } = event.target;
 
-    handleSubmitChatMessage = event => {
-        event.preventDefault();
+    this.setState({
+      chatInput: value
+    });
+  };
 
-        const newMessage = {
-            userName: "Pass Username here",
-            time: Date.now(),
-            message: this.state.chatInput
-        };
+  handleSubmitChatMessage = event => {
+    event.preventDefault();
 
-        this.setState({
-            messages: [...this.state.messages, newMessage] // Must match the objects in this.state.messages
-        });
-    }
+    const newMessage = {
+      userName: "Pass Username here",
+      time: Date.now(),
+      message: this.state.chatInput
+    };
 
-    render() {
-        return (
-            <div className="chatbox h-100 flx flx-row">
-                <div className="online-users">
-                    Online users here
-                </div>
-                <div className="chat-wrapper flx-grow-1">
-                    <div className="chat-messages">
-                        {this.state.messages.map(({userName, time, message}, i) =>     
-                            <ChatMessage key={i} userName={userName} time={time} message={message}/>
-                        )}
-                    </div>
-                    <form className="chat-form">
-                        <input onChange={this.handleInputChange} name="chatInput" value={this.state.chatInput} type="text" placeholder="Your message here"/>
-                        <button onClick={this.handleSubmitChatMessage}>Send</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+    this.setState({
+      messages: [...this.state.messages, newMessage] // Must match the objects in this.state.messages
+    });
+  };
+
+  render() {
+    return (
+      <div className="chatbox h-100 flx flx-row">
+        <div className="online-users">Online users here</div>
+        <div className="chat-wrapper flx-grow-1">
+          <div className="chat-messages">
+            {this.state.messages.map(({ userName, time, message }, i) => (
+              <ChatMessage
+                key={i}
+                userName={userName}
+                time={time}
+                message={message}
+              />
+            ))}
+          </div>
+          <form className="chat-form">
+            <input
+              onChange={this.handleInputChange}
+              name="chatInput"
+              value={this.state.chatInput}
+              type="text"
+              placeholder="Your message here"
+            />
+            <button onClick={this.handleSubmitChatMessage}>Send</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ChatBox;
