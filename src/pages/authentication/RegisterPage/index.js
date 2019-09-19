31346192b2a97cdd785a4373b4d915eedbdcf07e
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-19 13:34:09 -0400

    styles register form as well; fixes register form inputs

--- src/pages/authentication/RegisterPage/index.js
+++ src/pages/authentication/RegisterPage/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./RegisterPage.module.css";
 
 class RegisterPage extends React.Component {
   state = {
@@ -8,40 +9,55 @@ class RegisterPage extends React.Component {
     password: ""
   };
 
+  handleInputChange = event => {
+    const {name, value} = event.target;
+    this.setState({ [name]: value });
+  }
+
   handleSubmit = event => {
     event.preventDefault();
-    const {username, email, password} = event.target;
+    const { username, email, password } = event.target;
 
     console.log(username, email, password);
-  }
+  };
 
   render() {
     return (
-      <div id="authenticate-form-container">
-        <form>
-          <input
-            name="username"
-            onChange={this.handleInputChange}
-            value={this.state.username}
-          />
-          <input
-            name="email"
-            onChange={this.handleInputChange}
-            value={this.state.email}
-          />
-          <input
-            name="password"
-            onChange={this.handleInputChange}
-            value={this.state.password}
-            type="password"
-          />
-          <button onClick={this.handleSubmit}>Register</button>
-          <div>
-            <span>
-              Already have an account? <Link to="/">Login!</Link>
-            </span>
-          </div>
-        </form>
+      <div className={styles["register-page"]}>
+        <div className={styles["register-form-container"]}>
+          <form className={styles["register-form"]}>
+            <div className={styles["register-form-inputs"]}>
+              <input
+                className={styles["inputs"]}
+                name="username"
+                onChange={this.handleInputChange}
+                value={this.state.username}
+                placeholder="Username"
+              />
+              <input
+                className={styles["inputs"]}
+                name="email"
+                onChange={this.handleInputChange}
+                value={this.state.email}
+                placeholder="Email"
+              />
+              <input
+                className={styles["inputs"]}
+                name="password"
+                onChange={this.handleInputChange}
+                value={this.state.password}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <button className={styles["register-button"]} onClick={this.handleSubmit}>Register</button>
+            <div>
+              <span className={styles["already-have-account"]}>
+                Already have an account? <Link to="/">Login!</Link>
+              </span>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
