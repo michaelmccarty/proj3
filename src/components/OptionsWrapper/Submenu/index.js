de6163682383c1a166e147149c8d85d7cc232830
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-20 15:49:54 -0400

    fixes rendering issue; adds rendering for different sub menus; adds logout form

--- src/components/OptionsWrapper/Submenu/index.js
+++ src/components/OptionsWrapper/Submenu/index.js
@@ -1,6 +1,41 @@
 import React from "react";
 import styles from "./Submenu.module.css";
 
+function Pokemon(props) {
+    switch (props.id) { // id gets converted to a number when passing it down
+      case 1:
+        return (
+          <div className={styles["pokemon-wrapper"]}>
+            <img
+              className={styles["pokemon-image"]}
+              src="https://via.placeholder.com/50"
+            />
+            <span className={styles["pokemon-name"]}>Pokemon Name Pokemon</span>
+          </div>
+        );
+      case 2: 
+        return (
+          <div>These will be the Pokemon from the Pokedex</div>
+        )
+      case 3: 
+        return (
+          <div>These are the settings (icebox)</div>
+        )
+      case 4:
+        return (
+          <div className={styles["logout-submenu"]}>
+            Are you sure?
+            <button>Yes</button>
+            <button>Keep Playing</button>  
+          </div>
+        )
+      default: 
+        return (
+          <div>Not found</div>
+        );
+    } 
+}
+
 class Submenu extends React.Component {
   handleClose = event => {
     event.preventDefault();
@@ -13,10 +48,12 @@ class Submenu extends React.Component {
       <div className={styles["submenu-wrapper"]}>
         <div className={styles["submenu-header"]}>
           <span>{this.props.menuDisplayName}</span>
-          <button className={styles["exit-button"]} onClick={this.handleClose}>X</button>
+          <button className={styles["exit-button"]} onClick={this.handleClose}>
+            X
+          </button>
         </div>
-        <div>
-          Content
+        <div className={styles["content-container"]}>
+          <Pokemon id={this.props.id}/>
         </div>
       </div>
     );
