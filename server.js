1cd4252a01b8ef84d3f01bb1cc0c0e1106960ee1
Logan Miner <loganrobertminer@gmail.com>
2019-09-25 13:11:35 -0400

    client/build

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //serve up static assets (on heroku)
-if (process.env.NODE_ENV === "production") {app.use(express.static('client/public'));}
+if (process.env.NODE_ENV === "production") {app.use(express.static('client/build'));} 
+else {app.use(express.static('client/public'))}
 
 const LocalStrategy = require('passport-local').Strategy;
 
