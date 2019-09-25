bc4fdad0c85a62aa822db9a92c0139916b909de0
michaelmccarty <mccartyatl@gmail.com>
2019-09-25 10:42:23 -0400

    added code to logout button

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,36 +1,46 @@
 const bcrypt = require('bcrypt');
 
 module.exports = (app, passport, db) => {
+    app.post(
+        '/login',
+        passport.authenticate('local', {
+            // successRedirect: '/game',
+            failureRedirect: '/',
+            failerFlash: false
+        }),
+        function(req, res) {
+            res.json({ message: 'successful login' });
+        }
+    );
 
-  app.post('/login', passport.authenticate('local', {
-      // successRedirect: '/game',
-      failureRedirect: '/',
-      failerFlash: false
-  }), function (req, res) {
-    res.json({ message: "successful login" });
-  });
+    app.get('/logout', function(req, res) {
+        console.log(
+            '\n\n\n' + req.user.username + ' has been logged out.\n\n\n'
+        );
 
+        req.logout();
 
-  //register
-  app.post("/api/users", (req, res) => {
-    const unhashedpw = req.body.password;
-
-
-    console.log(req.body);
-
-
-    bcrypt.hash(unhashedpw, 10, function(err, hash) {
-
-      db.User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: hash
-      }).then(data => {
-        res.json({message: "registration successful"})}).catch(function (err){
-          console.log("registration error: "+err);
-        });
+        res.json({ message: 'successful logout' });
     });
 
+    //register
+    app.post('/api/users', (req, res) => {
+        const unhashedpw = req.body.password;
 
-  });
+        console.log(req.body);
+
+        bcrypt.hash(unhashedpw, 10, function(err, hash) {
+            db.User.create({
+                username: req.body.username,
+                email: req.body.email,
+                password: hash
+            })
+                .then(data => {
+                    res.json({ message: 'registration successful' });
+                })
+                .catch(function(err) {
+                    console.log('registration error: ' + err);
+                });
+        });
+    });
 };
